eda10bc787fae19f185cf9b856af5b597b9d5b98
Al3087 <alejandra.aviles2026@adoc.superate.org>
2026-09-24 12:51:40 -0600

    cambios de luces

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const btn = document.getElementById('btnMotor');
 const vel = document.getElementById('velocimetro');
+const luces = document.getElementById('luces');
+luces.style.backgroundColor = "#eab308";
+luces.style.color = "#000000";
+luces.textContent = "Luces: Encendidas (Modo Niebla)";
 btn.addEventListener('click', () => {
  vel.textContent = "Velocidad: 20 km/h (Marcha mínima)";
  btn.style.backgroundColor = "#10b981";
